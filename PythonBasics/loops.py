611c0c38e2b28d10c03c6706e46b284df0e6aad4
Rishabh Gupta <3rishabh3@gmail.com>
2023-08-05 15:50:55 +0530

    loops in python

---
+++ PythonBasics/loops.py
@@ -0,0 +1,18 @@
+
+# score = 0
+#
+# while score < 19:
+#     print('Your score is: ', score)
+#     score = score+1
+#
+# print("Hello there")
+
+
+# for i in 'HelloWorld':
+#     print("The letter is:", i)
+
+
+marvels = ['Deadpool', 'Thor', 'Hulk', 'Ironman', 'Spiderman']
+
+for hero in marvels:
+    print("Hero name is: ", hero)
