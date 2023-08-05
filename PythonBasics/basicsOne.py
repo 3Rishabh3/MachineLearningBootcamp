cf45a5c364127a9da63f25a1b99aaadbe6b07dca
Rishabh Gupta <3rishabh3@gmail.com>
2023-08-05 15:38:37 +0530

    python basics 1

---
+++ PythonBasics/basicsOne.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+
+score = "HelloWorld"
+
+print(score)
+print(score[2:])
+
+# lists
+myList = ["hulk", "spider man", "ironman", 3.7, 9, "superman"]
+print('list: ', myList[2:5])
+
+# tuples: readonly means can not be changed
+myTuple = ('text', 48)
+print('Tuple:', myTuple)
+
+# dictionaries: key-value pair
+myDict = {'name': 'superman', 'age': 40}
+
+print('Dict: ', myDict.keys())
